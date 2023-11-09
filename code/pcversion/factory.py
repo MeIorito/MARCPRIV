faab880c4ec8af4cfb7d16d239543f61a5f3a0c6
Melle Herlaar <90834076+MeIorito@users.noreply.github.com>
2023-11-09 14:16:09 +0100

    factory classes added

---
+++ code/pcversion/factory.py
@@ -0,0 +1,74 @@
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+from abc import ABC, abstractmethod
+
+
+class Factory(ABC):
+
+    def create(self):
+        pass
+
+
+class sliderFactory(Factory):
+    @staticmethod
+    def create(min, max, step, width, interval, style, function):
+        slider = QSlider()
+        slider.setMinimum(min)
+        slider.setMaximum(max)
+        slider.setPageStep(step)
+        slider.setFixedWidth(width)
+        slider.setTickInterval(interval)
+        slider.setStyleSheet(style)
+        slider.valueChanged.connect(function)
+        return slider
+    
+
+class labelFactory(Factory):
+    @staticmethod
+    def create(text, font, style):
+        label = QLabel(text)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        label.setFont(font)
+        label.setStyleSheet(style)
+        return label
+    
+
+class buttonFactory(Factory):
+    @staticmethod
+    def create(text, function, style, size):
+        button = QPushButton(text)
+        button.clicked.connect(function)
+        button.setStyleSheet(style)
+        button.setMinimumSize(size[0], size[1])
+        return button
+    
+class HBoxLayoutFactory(Factory):
+    @staticmethod
+    def create(*widgets):
+        layout = QHBoxLayout()
+
+        for widget in widgets:
+            layout.addWidget(widget)
+
+        return layout
+    
+
+class VBoxLayoutFactory(Factory):
+    @staticmethod
+    def create(*widgets):
+        layout = QVBoxLayout()
+
+        for widget in widgets:
+            layout.addWidget(widget)
+        
+        return layout
+    
+class QTableFactory(Factory):
+    @staticmethod
+    def create(size, tableStyle, scrollbarStyle):
+        table = QTableWidget()
+        table.setFixedSize(size[0], size[1])
+        table.setStyleSheet(tableStyle)
+        table.verticalScrollBar().setStyleSheet(scrollbarStyle)
+
+        return table
